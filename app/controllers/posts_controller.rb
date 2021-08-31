1d7d0a7168d53c57a8e3f79ece0f05a939b85229
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-01 00:50:10 +0900

    不備修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[shoe edit update destroy]
+  before_action :set_post, only: %i[show edit update destroy]
 
   def index
     @post = Post.order(id: :asc)
