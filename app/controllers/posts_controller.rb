391972cb00ef03b717bc4fdc9da9451296908ed8
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-01 00:19:09 +0900

    posts コントローラに処理を記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,17 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[shoe edit update destroy]
+
   def index
+    @post = Post.order(id: :asc)
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post
   end
 
   def show
@@ -15,8 +21,22 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post.update!(post_params)
+    redirect_to @post
   end
 
   def destroy
+    @post.destroy!
+    redirect_to @post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
