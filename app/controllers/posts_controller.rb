c841bfe4680c9e822de19392170ee21347c55bae
sekiguchi0215 <dsphere5296@gmail.com>
2021-09-01 01:17:57 +0900

    コントローラで受け取るパラメーターを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title)
+    params.require(:post).permit(:title, :image)
   end
 
   def set_post
